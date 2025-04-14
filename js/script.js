f92dc2d26b7a7c956fe6404c6acc72e3789068ec
paryaarezoo <paryaarezoo@gmail.com>
2025-04-15 01:57:39 +0330

    todoList init

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function main() {
         body.classList.toggle('dark-mode');
     }
 
-makeTodoList(JSON.parse(localStorage.getItem("todos")));
+    makeTodoList(JSON.parse(localStorage.getItem("todos")));
     addBtn.addEventListener("click", () => {
         const item = todoInput.value.trim();
 
@@ -40,7 +40,7 @@ makeTodoList(JSON.parse(localStorage.getItem("todos")));
             const currentTodo = {
                 item: item,
                 isCompleted: false
-            } //i didnt uunderstand why did i use obj here:))))
+            }
 
 
             todos.push(currentTodo);
@@ -61,99 +61,124 @@ makeTodoList(JSON.parse(localStorage.getItem("todos")));
 
 
 
-   
+
 
 
 }
+
 function makeTodoList(todoArray) {
     if (!todoArray) {
         return null;
     }
 
     todoArray.forEach(todoObj => {
-        const cardBody = document.createElement("li")
-        const card = document.createElement("div")
-        const cbInput = document.createElement("input")
-        const work = document.createElement("span")
-        const clear =document.createElement("button")
-        const iconTrash = document.createElement("i")
-        const iconPen = document.createElement("i")
- 
-
-
-        cardBody.classList.add("card-body")
-        cbInput.classList.add("cb-input","form-check-input")
-        work.classList.add("work")
-        clear.classList.add("clear")
-        iconTrash.classList.add("bi","bi-trash3")
-        iconTrash.id = "trash"
-        iconPen.classList.add("bi","bi-pen")
-
-        cardBody.appendChild(card)
-        card.appendChild(cbInput)
-        card.appendChild(work)
-        cardBody.appendChild(clear)
-        clear.appendChild(iconTrash)
-        clear.appendChild(iconPen)
-        document.querySelector(".todos").appendChild(cardBody);
+            const cardBody = document.createElement("li")
+            const card = document.createElement("div")
+            const cbInput = document.createElement("input")
+            const work = document.createElement("span")
+            const clear = document.createElement("button")
+            const iconTrash = document.createElement("i")
+            const iconPen = document.createElement("i")
 
 
 
+            cardBody.classList.add("card-body")
+            cbInput.classList.add("cb-input", "form-check-input")
+            work.classList.add("work")
+            clear.classList.add("clear")
+            iconTrash.classList.add("bi", "bi-trash3")
+            iconTrash.id = "trash"
+            iconPen.classList.add("bi", "bi-pen")
+
+            cardBody.appendChild(card)
+            card.appendChild(cbInput)
+            card.appendChild(work)
+            cardBody.appendChild(clear)
+            clear.appendChild(iconTrash)
+            clear.appendChild(iconPen)
+            document.querySelector(".todos").appendChild(cardBody);
 
 
 
 
 
-        cardBody.setAttribute("draggable", true)
-        cbInput.setAttribute("type","checkbox")
 
 
-        work.textContent = todoObj.item
+
+            cardBody.setAttribute("draggable", true)
+            cbInput.setAttribute("type", "checkbox")
 
 
-    
-        iconTrash.addEventListener("click", () => {
-            cardBody.remove()
+            work.textContent = todoObj.item
 
 
-        })
 
-        iconPen.addEventListener("click", () => {
-            const inputEdit = document.createElement("input")
-            inputEdit.classList.add("input-edit")
-            inputEdit.setAttribute("type", "text")
-            inputEdit.setAttribute("placeholder", "Edit Your Task")
-            inputEdit.value = work.textContent
+            iconTrash.addEventListener("click", () => {
+                cardBody.remove()
 
-            card.replaceChild(inputEdit, work)
-            inputEdit.focus()
 
-            inputEdit.addEventListener("keydown", (e) => {
-                if (e.key === "Enter") {
-                    const newValue = inputEdit.value.trim()
-                    work.textContent = newValue
-                    card.replaceChild(work, inputEdit)
+            })
 
-                }
+            iconPen.addEventListener("click", () => {
+                const inputEdit = document.createElement("input")
+                inputEdit.classList.add("input-edit")
+                inputEdit.setAttribute("type", "text")
+                inputEdit.setAttribute("placeholder", "Edit Your Task")
+                inputEdit.value = work.textContent
+
+                card.replaceChild(inputEdit, work)
+                inputEdit.focus()
+
+                inputEdit.addEventListener("keydown", (e) => {
+                    if (e.key === "Enter") {
+                        const newValue = inputEdit.value.trim()
+                        work.textContent = newValue
+                        card.replaceChild(work, inputEdit)
+
+                    }
+                })
+
+
+                const todos = JSON.parse(localStorage.getItem("todos"));
+                todos.forEach((todo) => {
+                    if (todo.item === work.textContent) {
+                        todo.item = newValue;
+                    }
+                });
+                localStorage.setItem("todos", JSON.stringify(todos));
+
             })
 
 
-            const todos = JSON.parse(localStorage.getItem("todos"));
-            todos.forEach((todo) => {
-                if (todo.item === work.textContent) {
-                    todo.item = newValue;
-                }
-            });
-            localStorage.setItem("todos", JSON.stringify(todos));
-            
-        })
+            cbInput.addEventListener("change", () => {
+                const todos = JSON.parse(localStorage.getItem("todos")) ;
+                todos.forEach((todo) => {
+                    if (todo.item === todoObj.item) {
+                        todo.isCompleted = cbInput.checked;
+                    }
+                })
+                localStorage.setItem("todos", JSON.stringify(todos));
+            })
 
+            if(todoObj.isCompleted){
+                cbInput.checked = true;
+                work.classList.add("done")
 
+            }
+            // if(cbInput.checked){
+            //     todo.isCompleted = true;
+            //     work.classList.add("done")
+
+            // }
+         
 
     });
 
 
+
+
 }
+
 
 
 
